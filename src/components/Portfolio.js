04149e722c2e4d3e960150b72e5b258cf2befb2e
TaikiCode <metoro5151@gmail.com>
2021-02-19 17:46:16 +0900

    change fortfolio comp

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,6 +3,7 @@ import ec from "../images/ec.png";
 import todo from "../images/todo.png";
 import coin_tracker from "../images/coin-tracker.png";
 import sns from "../images/sns.png";
+import portfolio from "../images/portfolio.png";
 // REACT POPUPBOX
 import "react-popupbox/dist/react-popupbox.css";
 import { PopupboxContainer } from "react-popupbox";
@@ -19,43 +20,40 @@ const Portfolio = () => {
       <div className="container">
         <h1 className="text-uppercase text-center py-4">Portfolio</h1>
 
-
-
-        <div className="row text-center my-5">
-
-          
-          <div className="col-lg-3 col-md-6">
-            <div className="card shadow-lg" onClick={openPopupboxEc}>
-              <img className="portfolio-image" src={ec} alt="todo app..." />
-            </div>
-          </div>
-
-          <div className="col-lg-3 col-md-6">
-            <div className="card shadow-lg" onClick={openPopupboxTodo}>
+        <div className="row justify-content-center mt-4">
+          <div className="col-lg-6 p-0">
+            <div className="card" onClick={openPopupboxEc}>
               <img
                 className="portfolio-image"
-                src={todo}
-                alt="todo app..."
+                src={portfolio}
+                alt="Portfolio Site"
               />
             </div>
           </div>
-
-          <div className="col-lg-3 col-md-6">
-            <div className="card shadow-lg" onClick={openPopupboxCoin}>
-              <div className="card-image">
-                <img
-                  className="portfolio-image"
-                  src={coin_tracker}
-                  alt="todo app..."
-                />
-              </div>
+          <div className="col-lg-6 p-0">
+            <div className="card" onClick={openPopupboxSns}>
+              <img className="portfolio-image" src={sns} alt="Sns App" />
             </div>
           </div>
-          <div className="col-lg-3 col-md-6">
-            <div className="card shadow-lg" onClick={openPopupboxSns}>
-              <div className="card-image">
-                <img className="portfolio-image" src={sns} alt="todo app..." />
-              </div>
+        </div>
+        <div className="row justify-content-center mb-4">
+          <div className="col-lg-4 p-0">
+            <div className="card" onClick={openPopupboxEc}>
+              <img className="portfolio-image" src={ec} alt="Ec Site" />
+            </div>
+          </div>
+          <div className="col-lg-4 p-0">
+            <div className="card" onClick={openPopupboxCoin}>
+              <img
+                className="portfolio-image"
+                src={coin_tracker}
+                alt="Coin_Trancker App"
+              />
+            </div>
+          </div>
+          <div className="col-lg-4 p-0">
+            <div className="card" onClick={openPopupboxTodo}>
+              <img className="portfolio-image" src={todo} alt="Todo App" />
             </div>
           </div>
         </div>
